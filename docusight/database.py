0e01e04834a63090fe2dc97564b5bd8f9599c2d3
P. M. Soliman <philipsoliman4133@gmail.com>
2025-09-23 13:07:34 +0200

    Added functionality for user entry persistance in database

--- docusight/database.py
+++ docusight/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from docusight.config import settings
-from docusight.models import Base
+from docusight.models import Base, User
 
 engine = create_async_engine(settings.DATABASE_URL, echo=False)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
@@ -18,6 +18,11 @@ async def create_tables():
         await conn.run_sync(Base.metadata.create_all)
 
 
-async def drop_tables():
+async def drop_tables(drop_users: bool = False):
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        if drop_users:
+            await conn.run_sync(User.__table__.drop)
+        else:
+            for table in Base.metadata.sorted_tables:
+                if table.name != User.__tablename__:
+                    await conn.run_sync(table.drop)

058b88a52f2b340cdffe3d2f7e686e1b46c23459
P. M. Soliman <philipsoliman4133@gmail.com>
2025-09-23 02:22:00 +0200

    Reformat

--- docusight/database.py
+++ docusight/database.py
@@ -1,18 +1,22 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from docusight.models import Base
+
 from docusight.config import settings
+from docusight.models import Base
 
 engine = create_async_engine(settings.DATABASE_URL, echo=False)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def get_db():
     async with async_session() as db:
         yield db
 
+
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
 
 async def drop_tables():
     async with engine.begin() as conn:
